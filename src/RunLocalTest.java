98cff0c4c4043cd9b004b0fb7900d00b62c0728c
kevin-zhang111 <88207736+kevin-zhang111@users.noreply.github.com>
2022-11-13 22:23:51 -0500

    Create RunLocalTest.java

---
+++ src/RunLocalTest.java
@@ -0,0 +1,68 @@
+import org.junit.Before;
+import org.junit.rules.Timeout;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import javax.swing.*;
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+import java.lang.reflect.InvocationTargetException;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+public class RunLocalTest {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(TestCase.class);
+        if (result.wasSuccessful()) {
+            System.out.println("Excellent - Test ran successfully");
+        } else {
+            for (Failure failure : result.getFailures()) {
+                System.out.println(failure.toString());
+            }
+        }
+    }
+
+    /**
+     * A set of public test cases.
+     *
+     * <p>Purdue University -- CS18000 -- Summer 2022</p>
+     *
+     * @author Purdue CS
+     * @version June 13, 2022
+     */
+    public static class TestCase {
+        private final PrintStream originalOutput = System.out;
+        private final InputStream originalSysin = System.in;
+
+        @SuppressWarnings("FieldCanBeLocal")
+        private ByteArrayInputStream testIn;
+
+        @SuppressWarnings("FieldCanBeLocal")
+        private ByteArrayOutputStream testOut;
+
+        @Before
+        public void outputStart() {
+            testOut = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(testOut));
+        }
+
+        @After
+        public void restoreInputAndOutput() {
+            System.setIn(originalSysin);
+            System.setOut(originalOutput);
+        }
+
+        private String getOutput() {
+            return testOut.toString();
+        }
+
+        @SuppressWarnings("SameParameterValue")
+        private void receiveInput(String str) {
+            testIn = new ByteArrayInputStream(str.getBytes());
+            System.setIn(testIn);
+        }
