c0bf12958911d97bec082572d3fbb760a6028c07
kevin-zhang111 <88207736+kevin-zhang111@users.noreply.github.com>
2022-11-13 23:31:10 -0500

    Update RunLocalTest.java

--- src/RunLocalTest.java
+++ src/RunLocalTest.java
@@ -77,5 +77,51 @@ public class RunLocalTest {
             assertEquals("Wrong", sList,user.customSplitSpecific(s));
 
         }
+
+        @Test(timeout = 1000)
+        public void testReadWholeFile() throws IOException {
+            User user1 = new User("a", "a@gmail.com", "123");
+            User user2 = new User("b", "b@gmail.com", "1234");
+            ArrayList<Message> mList1 = user1.readWholeFile();
+            ArrayList<Message> mList2 = user2.readWholeFile();
+            assertEquals( mList1.toString(), mList2.toString());
+        }
+
+        @Test(timeout = 1000)
+        public void testParseMessages() throws IOException {
+            User user1 = new User("Buyer1", "buyer1@purdue.edu", "password1234");
+            ArrayList<Message> mList = new ArrayList<>();
+            mList.add(new Message(169, "13-11-2022 02:23:31", "Buyer1", "Seller1", "Hi there", false, false));
+            mList.add(new Message(183, "13-11-2022 02:24:59", "Buyer1", "Seller1", "Yes i want to buy", false, false));
+            assertEquals( mList.toString(), user1.parseMessages().toString());
+        }
+
+        @Test(timeout = 1000)
+        public void testBlockUser(){
+            User user1 = new User("Buyer1", "buyer1@purdue.edu", "password1234");
+            User user2 = new User("a", "a@gmail.com", "123");
+            User user3 = new User("b", "b@gmail.com", "1234");
+            ArrayList<User> uList = new ArrayList<>();
+            uList.add(user1);
+            uList.add(user2);
+            uList.add(user3);
+            assertTrue(user1.blockUser("a", uList));
+            assertFalse(user1.blockUser("c", uList));
+        }
+
+        @Test(timeout = 1000)
+        public void testUnblockUser(){
+            User user1 = new User("Buyer1", "buyer1@purdue.edu", "password1234");
+            User user2 = new User("a", "a@gmail.com", "123");
+            User user3 = new User("b", "b@gmail.com", "1234");
+            ArrayList<User> uList = new ArrayList<>();
+            uList.add(user1);
+            uList.add(user2);
+            uList.add(user3);
+            user1.blockUser("a", uList);
+            user1.blockUser("c", uList);
+            assertTrue(user1.unblockUser("a", user1.getBlockedUsers()));
+            assertFalse(user1.unblockUser("c", user1.getBlockedUsers()));
+        }
     }
 }
